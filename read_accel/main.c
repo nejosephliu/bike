9cf2d5b0a636a23bf0ba6d43988cc7e028104d17
David Darevsky <40604189+ddarevsky@users.noreply.github.com>
2019-11-16 15:58:22 -0800

    added code to smooth acceleration data and added code to integrate acceleration to velocity

--- read_accel/main.c
+++ read_accel/main.c
@@ -19,9 +19,10 @@
 #include "nrf_twi_mngr.h"
 
 #include "app_timer.h"
-# include "nrf_drv_clock.h"
+#include "nrf_drv_clock.h"
 
 #include "buckler.h"
+#include "display.h"
 #include "mpu9250.h"
 
 // LED array on NRF (4 total)
@@ -71,11 +72,26 @@ void sliding_averager(mpu9250_measurement_t *input_array_pointer, mpu9250_measur
     temp_y /= (float) array_size;
     temp_z /= (float) array_size;
 
+    // Save smoothed values into
     output_array_pointer->x_axis = temp_x;
     output_array_pointer->y_axis = temp_y;
     output_array_pointer->z_axis = temp_z;
 
     return;
+}
+
+float update_velocity(mpu9250_measurement_t *smoothed_acceleration, int delta_t_msec, float previous_velocity) {
+    // TODO: Add an enum and switch/case statements for easy access selection
+    // For now just hardcoded to z-axis
+    float axis_accel_value = smoothed_acceleration->x_axis;
+    
+    if (axis_accel_value < .2)
+    {
+      return previous_velocity;
+    }
+
+    previous_velocity += (axis_accel_value * ((float) delta_t_msec / 1000.0));
+    return previous_velocity;
 }
 
 // TWI Manager Instances
@@ -114,17 +130,44 @@ int main(void) {
     error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
     APP_ERROR_CHECK(error_code);
 
+    // Start SPI for Kobuki display
+    nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+    nrf_drv_spi_config_t spi_config = {
+        .sck_pin = BUCKLER_LCD_SCLK,
+        .mosi_pin = BUCKLER_LCD_MOSI,
+        .miso_pin = BUCKLER_LCD_MISO,
+        .ss_pin = BUCKLER_LCD_CS,
+        .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+        .orc = 0,
+        .frequency = NRF_DRV_SPI_FREQ_4M,
+        .mode = NRF_DRV_SPI_MODE_2,
+        .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+    };
+    error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
+    APP_ERROR_CHECK(error_code);
+    display_init(&spi_instance);
+    display_write("Hello, Human!", DISPLAY_LINE_0);
+    printf("Display initialized!\n");
+
+
     // Allocate arrays for mpu9250 measurements
     mpu9250_measurement_t acc_array[16] = {0};
     mpu9250_measurement_t avg_output = {0};
 
     uint16_t measurement_array_counter = 0;
 
+    // Bike kinematics
+    float current_velocity = 0;
+
     // Start the timer
-    error_code = app_timer_start(accel_timer_id, APP_TIMER_TICKS(16), NULL);
+    int delta_t_msec = 16;
+    error_code = app_timer_start(accel_timer_id, APP_TIMER_TICKS(delta_t_msec), NULL);
     APP_ERROR_CHECK(error_code);
 
+    // Call MPU9250 init code with the I2C manager instance
     mpu9250_init(&twi_mngr_instance);
+
+    uint8_t print_counter = 0;
 
     // loop forever
     while (1) {
@@ -134,10 +177,16 @@ int main(void) {
             acc_array[measurement_array_counter % 16] = mpu9250_read_accelerometer();
             measurement_array_counter++;
             sliding_averager(acc_array, &avg_output, sizeof(acc_array) / sizeof(mpu9250_measurement_t));
-            //printf("X: %f, Y: %f, Z: %f\n", acc_red.x_axis, acc_red.y_axis, acc_red.z_axis);
-            printf("X Axis Smoothed: %f\n Y Axis Smoothed: %f \nZ Axis Smoothed: %f\n\n", avg_output.x_axis,
-                                                                                          avg_output.y_axis, 
-                                                                                          avg_output.z_axis);
+            // printf("X: %f, Y: %f, Z: %f\n", acc_red.x_axis, acc_red.y_axis, acc_red.z_axis);
+            // printf("X Axis Smoothed: %f\n Y Axis Smoothed: %f \nZ Axis Smoothed: %f\n\n", avg_output.x_axis,
+            //        avg_output.y_axis,
+            //        avg_output.z_axis);
+            current_velocity = update_velocity(&avg_output, delta_t_msec, current_velocity);
+
+            if((print_counter % 20) == 0) {
+                printf("Current Velocity: %f\n", current_velocity);
+            }
+            print_counter++;
         }
     }
 }
