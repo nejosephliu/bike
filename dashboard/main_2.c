c7ce60833f5e079b2ddf2e6cec3ceb782f68d1c1
David Darevsky <40604189+ddarevsky@users.noreply.github.com>
2019-12-08 20:37:13 -0800

    added more to main V2.c

--- dashboard/main_2.c
+++ dashboard/main_2.c
@@ -23,11 +23,18 @@
 #include "led_strip.h"
 #include "led_pattern.h"
 #include "grove_display.h"
-#include "gyro.h"
 #include "IMU.h"
 #include "quaternionFilters.h"
 #include "si7021.h"
+#include <math.h>
 
+// Constants:
+
+// Number of AHRS readings that we smooth
+#define smooth_num 300
+#define PI 3.14159265359
+
+// GPIO defines
 #define LED_PWM NRF_GPIO_PIN_MAP(0, 17)     // GPIO pin to control LED signal
 
 // Buttons to select state instead of the voice detector (for now)
@@ -38,7 +45,7 @@
 // Put the buttons into an array
 static uint8_t BUTTONS[3] = {NRF_BUTTON0, NRF_BUTTON1, NRF_BUTTON2};
 
-/*For now, let's create a single timer that we will use to 
+/*For now, let's create a single timer that we will use to
 get the velocity from the Hall sensor and get the delta-T for the
 AHRS algo.
 */
@@ -49,14 +56,43 @@ volatile int hall_effect_mag_detects = 0;
 APP_TIMER_DEF(hall_velocity_calc);
 
 float hall_effect_timer_callback(void *p_context) {
-	// Code to get velocity
-	hall_effect_mag_detects = 0;
-	// return velocity (as float)
+    // Code to get velocity
+    hall_effect_mag_detects = 0;
+    // return velocity (as float)
 }
 
 void init_hall_effect_timer(void) {
-	
+    // Enalbe low frequency clock for timers
+    // Do this *once* in the entire program
+    ret_code_t error_code = nrf_drv_clock_init();
+    APP_ERROR_CHECK(error_code);
+    nrf_drv_clock_lfclk_request(NULL);
+
+    error_code = app_timer_create(&hall_velocity_calc,
+                                  APP_TIMER_MODE_REPEATED,
+                                  hall_effect_timer_callback);
+    APP_ERROR_CHECK(error_code);
 }
+
+
+volatile bool IMU_data_ready = false;
+// IMU interrupt callback function
+void IMU_interrupt_callback(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
+    IMU_data_ready = true;
+}
+
+// Function to setup read of mpu9250 on an interrupt
+void setup_IMU_interrupt(void) {
+    nrfx_gpiote_in_config_t IMU_int_config = NRFX_GPIOTE_CONFIG_IN_SENSE_LOTOHI(true);
+    error_code = nrfx_gpiote_in_init(BUCKLER_IMU_INTERUPT, &IMU_int_config, IMU_interrupt_callback);
+    APP_ERROR_CHECK(error_code);
+    nrfx_gpiote_in_event_enable(BUCKLER_IMU_INTERUPT, true);
+}
+
+// Function to setup the 3 state buttons as input pins
+
+
+// Callback function for 3 buttons to check state
 
 // Create TWI manager instance to read the IMU
 NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
