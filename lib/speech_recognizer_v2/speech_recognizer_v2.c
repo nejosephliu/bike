9758a7f64cacd5b021c26fd987371927768f9f56
David Darevsky <40604189+ddarevsky@users.noreply.github.com>
2019-12-10 11:43:25 -0800

    fixed merge conflicts in control with speech_recognizer_v2.c

--- lib/speech_recognizer_v2/speech_recognizer_v2.c
+++ lib/speech_recognizer_v2/speech_recognizer_v2.c
@@ -26,53 +26,69 @@ NRF_SERIAL_UART_DEF(serial_uart, 0);
 
 // Assumes app timer has already been started
 void speech_init(void) {
-  ret_code_t ret = nrf_serial_init(&serial_uart, &uart_config, &serial_config);
-  APP_ERROR_CHECK(ret);
+    ret_code_t ret = nrf_serial_init(&serial_uart, &uart_config, &serial_config);
+    APP_ERROR_CHECK(ret);
 }
 
 uint8_t speech_read(void) {
-  ret_code_t ret;
-  uint8_t input_number = 0;
-  ret = nrf_serial_read(&serial_uart, &input_number, sizeof(input_number), NULL, 0);
+    ret_code_t ret;
+    uint8_t input_number = 0;
+    ret = nrf_serial_read(&serial_uart, &input_number, sizeof(input_number), NULL, 0);
 
-<<<<<<< HEAD
-	if(ret == NRF_SUCCESS) {
-		return input_number;
-	} else {
-		return 255;
-	}
-=======
-  if (ret == NRF_SUCCESS) {
-	return input_number;
-  }
-  return 255;
+    if(ret == NRF_SUCCESS) {
+        return input_number;
+    } else {
+        return 255;
+    }
+    
 }
->>>>>>> master
 
-const char* speech_convert_reading(uint8_t reading) {
-  switch (reading) {
-  case 0x01: return "Turn on the light";
-  case 0x02: return "Turn off the light";
-  case 0x03: return "Play music";
-  case 0x04: return "Pause";
-  case 0x05: return "Next";
-  case 0x06: return "Previous";
-  case 0x07: return "Up";
-  case 0x08: return "Down";
-  case 0x09: return "Turn on the TV";
-  case 0x0a: return "Turn off the TV";
-  case 0x0b: return "Increase Temperature";
-  case 0x0c: return "Decrease Temperature";
-  case 0x0d: return "What's the time";
-  case 0x0e: return "Open the door";
-  case 0x0f: return "Close the door";
-  case 0x10: return "Left";
-  case 0x11: return "Right";
-  case 0x12: return "Stop";
-  case 0x13: return "Start";
-  case 0x14: return "Mode 1";
-  case 0x15: return "Mode 2";
-  case 0x16: return "Go";
-  }
-  return "Invalid";
+const char *speech_convert_reading(uint8_t reading) {
+    switch (reading) {
+    case 0x01:
+        return "Turn on the light";
+    case 0x02:
+        return "Turn off the light";
+    case 0x03:
+        return "Play music";
+    case 0x04:
+        return "Pause";
+    case 0x05:
+        return "Next";
+    case 0x06:
+        return "Previous";
+    case 0x07:
+        return "Up";
+    case 0x08:
+        return "Down";
+    case 0x09:
+        return "Turn on the TV";
+    case 0x0a:
+        return "Turn off the TV";
+    case 0x0b:
+        return "Increase Temperature";
+    case 0x0c:
+        return "Decrease Temperature";
+    case 0x0d:
+        return "What's the time";
+    case 0x0e:
+        return "Open the door";
+    case 0x0f:
+        return "Close the door";
+    case 0x10:
+        return "Left";
+    case 0x11:
+        return "Right";
+    case 0x12:
+        return "Stop";
+    case 0x13:
+        return "Start";
+    case 0x14:
+        return "Mode 1";
+    case 0x15:
+        return "Mode 2";
+    case 0x16:
+        return "Go";
+    }
+    return "Invalid";
 }
